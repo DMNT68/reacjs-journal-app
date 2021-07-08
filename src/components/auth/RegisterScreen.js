8256e2a513b4b1aa128a27cbb12fc5a53bb1033a
Andrés Salgado <andressalgado41@gmail.com>
2021-07-07 23:53:51 -0500

    sass

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+            <form>
+                <input className="auth__input" type="text" placeholder="Name" name="name" autoComplete="off"/>
+                <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete="off"/>
+                <input className="auth__input" type="password" placeholder="Password" name="password" autoComplete="off"/>
+                <input className="auth__input" type="password" placeholder="Confirm Password" name="password2" autoComplete="off"/>
+                <button className="btn btn-primary btn-block mb-5" type="submit">Register</button>
+                
+                
+                <Link className="link mt-5" to="/auth/login">Already registered?</Link>
+            </form>
+        </>
     )
 }
